ee36674ba720eae443eaf5ed2930711c17561dac
Top <chudeng@hanmail.net>
2021-02-26 16:39:10 +0900

    updated

--- ch4/4-2/iris-train.py
+++ ch4/4-2/iris-train.py
@@ -22,39 +22,39 @@ with open('iris.csv', 'r', encoding='utf-8') as fp:
         csv.append(cols)
 
 
-    # 가장 앞 줄의 헤더 제거
-    del csv[0]
+# 가장 앞 줄의 헤더 제거
+del csv[0]
 
 
-    # 데이터 셔플하기
-    random.shuffle(csv)
+# 데이터 셔플하기
+random.shuffle(csv)
 
 
-    # 학습 전용 데이터와 테스트 전용 데이터 분할하기
-    total_len = len(csv)
-    train_len = int(total_len * 2 / 3)
-    train_data = []
-    train_label = []
-    test_data = []
-    test_label = []
+# 학습 전용 데이터와 테스트 전용 데이터 분할하기
+total_len = len(csv)
+train_len = int(total_len * 2 / 3)
+train_data = []
+train_label = []
+test_data = []
+test_label = []
 
-    for i in range(total_len):
-        data = csv[i][0:3]
-        label = csv[i][4]
-        if i < train_len:
-            train_data.append(data)
-            train_label.append(label)
-        else:
-            test_data.append(data)
-            test_label.append(data)
-
-
-    # 데이터를 학습시키고 예측하기
-    clf = svm.SVC()
-    clf.fit(train_data, train_label)
-    pre = clf.predict(test_data)
+for i in range(total_len):
+    data = csv[i][0:3]
+    label = csv[i][4]
+    if i < train_len:
+        train_data.append(data)
+        train_label.append(label)
+    else:
+        test_data.append(data)
+        test_label.append(data)
 
 
-    # 정답을 구하기
-    ac_score = metrics.accuracy_score(test_label, pre)
-    print('정답률 = ', ac_score)
+# 데이터를 학습시키고 예측하기
+clf = svm.SVC()
+clf.fit(train_data, train_label)
+pre = clf.predict(test_data)
+
+
+# 정답을 구하기
+ac_score = metrics.accuracy_score(test_label, pre)
+print('정답률 = ', ac_score)
