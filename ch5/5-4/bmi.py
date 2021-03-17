9c6e8e07ec270110be0df41265baf3ffbe72c6c5
Top <chudeng@hanmail.net>
2021-03-17 13:47:09 +0900

    gogo

--- ch5/5-4/bmi.py
+++ ch5/5-4/bmi.py
@@ -6,14 +6,14 @@ import tensorflow as tf
 csv = pd.read_csv('bmi.csv')
 # 데이터 정규화
 csv['height'] = csv['height'] / 200
-csv['weight'] = csv['weight'] / 100
+csv['weight'] = csv['weight'] / 200
 # 레이블을 배열로 변환하기
 # - thin = (1, 0, 0) / normal = (0, 1, 0) / fat = (0, 0, 1)
 bclass = {'thin': [1, 0, 0], 'normal': [0, 1, 0], 'fat': [0, 0, 1]}
 csv['label_pat'] = csv['label'].apply(lambda x : np.array(bclass[x]))
 
 # 테스트를 위한 데이터 분류
-test_csv = csv[15000 : 20000]
+test_csv = csv[15000:20000]
 test_pat = test_csv[['weight', 'height']]
 test_ans = list(test_csv['label_pat'])
 
@@ -23,13 +23,13 @@ x = tf.placeholder(tf.float32, [None, 2]) # 키와 몸무게 데이터 넣기
 y_ = tf.placeholder(tf.float32, [None, 3]) # 정답 레이블 넣기
 
 # 변수 선언하기
-W = tf.Variable(tf.zeros([2, 3])); # 가중치
+w = tf.Variable(tf.zeros([2, 3])); # 가중치
 b = tf.Variable(tf.zeros([3])); # 바이어스
 # 소프트맥스 회귀 정의하기
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+y = tf.nn.softmax(tf.matmul(x, w) + b)
 
 # 모델 훈련하기
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+cross_entropy = -tf.reduce_sum(y_* tf.log(y))
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(cross_entropy)
 
@@ -42,12 +42,12 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer()) # 변수 초기화하기
 # 학습시키기
 for step in range(3500):
-    i = (step * 100) % 14000
-    rows = csv[1 + i : 1 + i + 100]
+    i = (step * 100) %14000
+    rows = csv[1 + i : 1 + i +100]
     x_pat = rows[['weight', 'height']]
     y_ans = list(rows['label_pat'])
     fd = {x: x_pat, y_: y_ans}
-    sess.run(train, feed_dict = fd)
+    sess.run(train, feed_dict=fd)
     if step % 500 == 0:
         cre = sess.run(cross_entropy, feed_dict = fd)
         acc = sess.run(accuracy, feed_dict = {x: test_pat, y_: test_ans})
--- ch5/5-5/tb-add.py
+++ ch5/5-5/tb-add.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
-# 상수와 변수 선언하
-a = tf.constant(100, name='a')
-b = tf.constant(200, name='b')
-c = tf.constant(300, name='c')
-v = tf.Variable(0, name='v')
+# 상수와 변수 선언하기
+a = tf.constant(100, name = 'a')
+b = tf.constant(200, name = 'b')
+c = tf.constant(300, name = 'c')
+v = tf.Variable(0, name = 'v')
 
-# 곱셉을 수행하는 그래프 정의하기
-calc_op = a + b * c
+# 곱셈을 수행하는 그래프 정의하기
+calc_op = a + b + c
 assign_op = tf.assign(v, calc_op)
 
 # 세션 생성하기
